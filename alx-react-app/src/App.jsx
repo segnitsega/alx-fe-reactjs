ae264ac6fdad22351a82a26ae3fe9ccffaf05d66
Segni <segnitsega6@gmail.com>
2024-08-15 09:48:46 -0700

    added Footer, header, and MainCOntent

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header/Header';
+import MainContent from './MainContent/MainContent';
+import Footer from './Footer/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +33,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
