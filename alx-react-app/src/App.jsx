039bd48c3f0ed3b90d4b5adfc09f87f8050f2b67
Segni <segnitsega6@gmail.com>
2024-08-15 11:44:03 -0700

    added UserProfile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,6 +38,11 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
     </>
   )
 }
