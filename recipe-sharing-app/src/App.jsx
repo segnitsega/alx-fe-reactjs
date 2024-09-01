f01f17f69c5f109dd68c0bfb4dcdb2149d0e2f1a
Segni <segnitsega6@gmail.com>
2024-09-01 09:40:12 -0700

    initialized zustand project

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import RecipeList from './components/RecipeList'
+import AddRecipeList from './components/AddRecipeForm'
+
 function App() {
   const [count, setCount] = useState(0)
 
