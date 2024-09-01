632e114fb645c6acde335da232933fc00f966e0f
Segni <segnitsega6@gmail.com>
2024-09-01 10:01:18 -0700

    second task

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,25 +1,16 @@
-// src/components/RecipeDetails.jsx
-import { useParams } from 'react-router-dom';
-import useRecipeStore from '../recipeStore';
-import EditRecipeForm from './EditRecipeForm';
-import DeleteRecipeButton from './DeleteRecipeButton';
+ // RecipeDetails component
+ import { useRecipeStore } from './recipeStore';
 
-const RecipeDetails = () => {
-  const { recipeId } = useParams();
-  const recipe = useRecipeStore(state => 
-    state.recipes.find(recipe => recipe.id === parseInt(recipeId))
-  );
+ const RecipeDetails = ({ recipeId }) => {
+   const recipe = useRecipeStore(state =>
+     state.recipes.find(recipe => recipe.id === recipeId)
+   );
 
-  if (!recipe) return <p>Recipe not found.</p>;
-
-  return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <p>{recipe.description}</p>
-      <EditRecipeForm recipe={recipe} />
-      <DeleteRecipeButton recipeId={recipe.id} />
-    </div>
-  );
-};
-
-export default RecipeDetails;
+   return (
+     <div>
+       <h1>{recipe.title}</h1>
+       <p>{recipe.description}</p>
+       {/* Render EditRecipeForm and DeleteRecipeButton here */}
+     </div>
+   );
+ };
