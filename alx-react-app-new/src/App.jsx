e9d87778e544b00f3b73486b4de3b7a6de028a07
Segni <segnitsega6@gmail.com>
2024-09-01 03:10:58 -0700

    implemented useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -43,6 +43,7 @@ function App() {
         age="25" 
         bio="Loves hiking and photography" 
       />
+      <Counter />
     </>
   )
 }
