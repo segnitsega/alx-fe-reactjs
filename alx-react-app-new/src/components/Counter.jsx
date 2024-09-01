e36666970662b2fc4a6ee599565471d1f6541f1c
Segni <segnitsega6@gmail.com>
2024-09-01 03:13:33 -0700

    implemented useState hook

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -7,7 +7,7 @@ function Counter() {
         <p>Current Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}> Decrement</button>
-      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   )
 }
